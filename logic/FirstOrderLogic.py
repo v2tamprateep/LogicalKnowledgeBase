75bd04b866255a465e9c57b3ecd4bff99064f665
Virin Tamprateep <v2tamprateep@gmail.com>
2020-09-06 08:58:28 -0700

    Tests for nested quantifiers

--- logic/FirstOrderLogic.py
+++ logic/FirstOrderLogic.py
@@ -42,10 +42,6 @@ class Quantifier(Sentence):
         return {v: e for v, e in tuples}
 
     def _normalize(self, existing_bindings: Mapping[str, Entity]) -> str:
-        if len(existing_bindings) == 0:
-            # no need to normalize
-            return
-
         existing_variables = [existing_bindings.keys()]
         normalized_vars = []
 
@@ -53,6 +49,7 @@ class Quantifier(Sentence):
             if not variable in existing_variables:
                 normalized_vars.append(variable)
                 existing_variables.append(variable)
+                continue
 
             count = 1
             while f'{variable}{count}' in existing_variables:
@@ -62,18 +59,20 @@ class Quantifier(Sentence):
             normalized_vars.append(variable)
             existing_variables.append(variable)
 
-        self._update_variables(normalized_vars)
+        self.update_variables(normalized_vars)
 
     def evaluate(self,
                  entities: Mapping[str, Entity],
                  functions: Iterable[Function],
                  bindings: Mapping[str, Entity]=dict()) -> bool:
-        self._normalize(bindings)
+        # If we already have bindings, ensure there are no conflicting variables
+        # if len(bindings) != 0:
+        #     self._normalize(bindings)
 
         permutations = itertools.permutations(entities.values(), len(self._variables))
         return self.evaluate_permutations(permutations, entities, functions, bindings)
 
-    def _update_variables(self, variables: Iterable[str]):
+    def update_variables(self, variables: Iterable[str]):
         self._variables = variables
         self._sentence.update_variables(variables)
 
@@ -114,7 +113,8 @@ class UniversalQuantifier(Quantifier):
             new_bindings = Quantifier._create_bindings(self._variables, permutation)
             new_bindings.update(bindings)
 
-            if not self._sentence.to_predicate().evaluate(entities, functions, new_bindings):
+            predicate = self._sentence.to_predicate()
+            if not predicate.evaluate(entities, functions, new_bindings):
                 return False
 
         return True
--- tests/test_firstorderlogic.py
+++ tests/test_firstorderlogic.py
@@ -9,7 +9,25 @@ def entity_list_to_dictionary(entities: Iterable[Entity]) -> Mapping[str, Entity
         return {e.name: e for e in entities}
 
 
-class EvaluationTests(TestCase):
+class FunctionTests(TestCase):
+
+    def test_function_evaluation(self):
+        alice = Entity('Alice')
+        bob = Entity('Bob')
+
+        func = Function('isAlice', ['p1, p2'])
+
+        func1 = Function('isAlice', ['p1', 'p3'])
+        func2 = Function('isBob', ['p1, p2'])
+        # Identical function but different instance
+        func3 = Function('isAlice', ['p1, p2'])
+
+        self.assertFalse(func.evaluate(entities=dict(), functions=[func1]))
+        self.assertFalse(func.evaluate(entities=dict(), functions=[func2]))
+        self.assertTrue(func.evaluate(entities=dict(), functions=[func3]))
+
+
+class NestedQuantifierTests(TestCase):
 
     def test_existentialquantifier_evaluation(self):
         alice = Entity('Alice')
@@ -26,20 +44,55 @@ class EvaluationTests(TestCase):
         functions = [Function('areFriends', ['Alice', 'Bob'])]
         self.assertTrue(quantifier.evaluate(entities, functions))
 
-    def test_function_evaluation(self):
+    def test_nested_existentialquantifier_evaluation(self):
         alice = Entity('Alice')
         bob = Entity('Bob')
+        carol = Entity('Carol')
 
-        func = Function('isAlice', ['p1, p2'])
+        # Construct ExEy.AreFriends(x, y)
+        are_friends = Function('areFriends', ['p1', 'p2'])
+        inner_quantifier = ExistentialQuantifier(['p2'], are_friends)
+        outer_quantifier = ExistentialQuantifier(['p1'], inner_quantifier)
 
-        func1 = Function('isAlice', ['p1', 'p3'])
-        func2 = Function('isBob', ['p1, p2'])
-        # Identical function but different instance
-        func3 = Function('isAlice', ['p1, p2'])
+        entities = entity_list_to_dictionary([alice, bob, carol])
 
-        self.assertFalse(func.evaluate(entities=dict(), functions=[func1]))
-        self.assertFalse(func.evaluate(entities=dict(), functions=[func2]))
-        self.assertTrue(func.evaluate(entities=dict(), functions=[func3]))
+        self.assertFalse(outer_quantifier.evaluate(entities, functions=[]))
+
+        functions = [Function('areFriends', ['Alice', 'Bob'])]
+        self.assertTrue(outer_quantifier.evaluate(entities, functions))
+
+    def test_nested_mixed_quantifiers_evaluation(self):
+        pass
+
+    def test_nested_universalquantifier_evaluation(self):
+        alice = Entity('Alice')
+        bob = Entity('Bob')
+        carol = Entity('Carol')
+
+        # Construct AxAy.AreFriends(x, y)
+        are_friends = Function('areFriends', ['p1', 'p2'])
+        inner_quantifier = UniversalQuantifier(['p2'], are_friends)
+        outer_quantifier = UniversalQuantifier(['p1'], inner_quantifier)
+
+        entities = entity_list_to_dictionary([alice, bob, carol])
+        functions = []
+        self.assertFalse(outer_quantifier.evaluate(entities, functions))
+
+        functions.extend(
+            [Function('areFriends', ['Alice', 'Bob']),
+            [Function('areFriends', ['Alice', 'Carol']),
+            Function('areFriends', ['Bob', 'Carol']),
+            Function('areFriends', ['Bob', 'Alice']),
+            Function('areFriends', ['Carol', 'Alice']),
+            Function('areFriends', ['Carol', 'Bob'])])])
+        self.assertFalse(outer_quantifier.evaluate(entities, functions))
+
+        # Everyone also has to be friends with themselves, which is rather wholesome
+        functions.extend(
+            [Function('areFriends', ['Alice', 'Alice']),
+            Function('areFriends', ['Bob', 'Bob']),
+            Function('areFriends', ['Carol', 'Carol'])])
+        self.assertTrue(outer_quantifier.evaluate(entities, functions))
 
     def test_universalquantifier_evaluation(self):
         alice = Entity('Alice')
@@ -64,29 +117,3 @@ class EvaluationTests(TestCase):
             Function('areFriends', ['Bob', 'Alice']),
             Function('areFriends', ['Carol', 'Bob'])])
         self.assertTrue(quantifier.evaluate(entities, functions))
-
-
-class NestedQuantifierTests(TestCase):
-
-    def test_nested_existentialquantifier(self):
-        alice = Entity('Alice')
-        bob = Entity('Bob')
-        carol = Entity('Carol')
-
-        # Construct something along the lines of: ExEy.AreFriends(x, y)
-        are_friends = Function('areFriends', ['p1', 'p2'])
-        inner_quantifier = ExistentialQuantifier(['p2'], are_friends)
-        outer_quantifier = ExistentialQuantifier(['p1'], inner_quantifier)
-
-        entities = entity_list_to_dictionary([alice, bob, carol])
-
-        self.assertFalse(outer_quantifier.evaluate(entities, functions=[]))
-
-        functions = [Function('areFriends', ['Alice', 'Bob'])]
-        self.assertTrue(outer_quantifier.evaluate(entities, functions))
-
-    def test_nested_mixed_quantifiers(self):
-        pass
-
-    def test_nested_universalquantifier(self):
-        pass
